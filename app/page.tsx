effb7c899cafe51c773e344b5148cf0e84653ef3
night-vw <waw78075@gmail.com>
2024-12-03 16:27:44 +0900

    add:Connect-supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import React from 'react'
+import { supabase } from '@/utils/supabaseClient'
 
-export default page
+export default async function Page() {
+  const supabaseClient = supabase();
+  const { data } = await supabaseClient.from('slot_data').select('*');
+
+  return (
+    <div>
+      <h1>Supabase Data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
